fe9456627b88f9b6b409d31593eb8a3d951d0b04
LasBoi <Azlj1689gk>
2019-09-19 12:02:50 +0200

    Oprettet metode til at tjekke ens gæt

--- src/Opgave_3_14/MyClass.java
+++ src/Opgave_3_14/MyClass.java
@@ -6,13 +6,33 @@ public class MyClass {
 
     static int randomNumber;
     static int guessedNumber;
+    static String headTails;
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        randomNumber = (int) (Math.random());
-        System.out.println(randomNumber);
+        starGenerater();
+        System.out.println("** Skriv et tal mellem 1 & 2: ");
+        guessedNumber = scanner.nextInt();
+        starGenerater();
+        System.out.println("** Dit gæt er: " + guessedNumber);
+        starGenerater();
+        randomNumber = (int) (Math.random() * 2);
+        System.out.println("** Vi generere nu et tal: " + randomNumber + " ("+ headTails + ")");
+        checkGuess();
 
+    }
+
+    static void checkGuess() {
+        if (guessedNumber == randomNumber)
+            System.out.println("** Du gættede rigtigt!");
+        else
+            System.out.println("** Desværre men du tog fucking fejl");
+    }
+
+
+    static void starGenerater() {
+        System.out.println("******************************");
     }
 }
